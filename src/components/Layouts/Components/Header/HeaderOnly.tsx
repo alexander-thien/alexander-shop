7aef5bd2a022851f74996aad7ece8f24f4f452ff
Thien Tu <thientu.work@gmail.com>
2022-07-28 16:32:46 +0700

    Create Login page

--- src/components/Layouts/Components/Header/HeaderOnly.tsx
+++ src/components/Layouts/Components/Header/HeaderOnly.tsx
@@ -1,25 +1,31 @@
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
+import HeaderWrapper from '.';
 
 function HeaderOnly() {
 	const { state } = useLocation();
 	return (
-		<div className='flex items-center h-[100%] mx-auto w-[80%] justify-between'>
-			<h1 className='font-extrabold text-xl'>AlexanderShop</h1>
-			{state === null ? (
-				<Link
-					to='/register'
-					state={{ register: true }}
-					className='font-normal'
-				>
-					Get stated
+		<HeaderWrapper>
+			<div className='flex items-center h-[100%] mx-auto w-[80%] justify-between'>
+				<Link to='/'>
+					<h1 className='font-extrabold text-xl'>AlexanderShop</h1>
 				</Link>
-			) : (
-				<Link to='/login' className='font-normal'>
-					Login
-				</Link>
-			)}
-		</div>
+
+				{state === null ? (
+					<Link
+						to='/register'
+						state={{ register: true }}
+						className='font-normal'
+					>
+						Get stated
+					</Link>
+				) : (
+					<Link to='/login' className='font-normal'>
+						Login
+					</Link>
+				)}
+			</div>
+		</HeaderWrapper>
 	);
 }
 
--- src/components/Layouts/OnlyHeader/index.tsx
+++ src/components/Layouts/OnlyHeader/index.tsx
@@ -1,14 +1,13 @@
 import HeaderOnly from '../Components/Header/HeaderOnly';
 import { Props } from '../../Types';
-import HeaderWrapper from '../Components/Header';
 
 const OnlyHeader: React.FC<Props> = ({ children }) => {
 	return (
 		<div>
-			<HeaderWrapper>
-				<HeaderOnly />
-			</HeaderWrapper>
-			<div className='out_side-content'>{children}</div>
+			<HeaderOnly />
+			<div className='bg-[#f8f8fa] w-[100%] h-[calc(100vh-64px)]'>
+				{children}
+			</div>
 		</div>
 	);
 };
